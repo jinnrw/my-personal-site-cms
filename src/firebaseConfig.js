32e7fbfd8e57457eb0466380e0c186f4a824a728
Jinn Wang <jinn.r.w@gmail.com>
2018-08-03 10:36:36 -0700

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyD7GDnC3SIpC6rmtTFLFtPhK5UgH08zfbc",
-  authDomain: "jinnwang-site.firebaseapp.com",
-  databaseURL: "https://jinnwang-site.firebaseio.com",
-  projectId: "jinnwang-site",
-  storageBucket: "jinnwang-site.appspot.com",
-  messagingSenderId: "268925780444"
-}
